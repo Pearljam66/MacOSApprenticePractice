cbd31def8c476bf3e48a89a25b3619d94083a77b
Sarah Clark <sarahireneclark@gmail.com>
2024-04-24 14:12:57 -0500

    Initial commit of Snowman project

---
+++ Snowman/Snowman/SnowmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowmanApp.swift
+//  Snowman
+//
+//  Created by Sarah Clark on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
